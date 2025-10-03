f6b819b37698bddd83a7e36219dc2a6d7e744969
jinyus <starboymc4@gmail.com>
2025-10-03 18:18:47 -0500

    refactor: remove unused dependencies and update project structure

--- rgen.py
+++ rgen.py
@@ -4,11 +4,29 @@ import click
 from nanoid import generate
 
 
-@click.command('Generate random string with nanoid')
-@click.option('--size', '-s', default=10, help='Size of the string', show_default=True)
-@click.option('--alphabet', '-a', default=string.ascii_lowercase + string.digits, help='Alphabet to use', show_default=True)
-@click.option('--use-punctuation', '-p', default=False, is_flag=True, help='Use punctuation in alphabet')
-@click.option('--use-uppercase', '-u', default=False, is_flag=True, help='Use uppercase in alphabet')
+@click.command("Generate random string with nanoid")
+@click.option("--size", "-s", default=10, help="Size of the string", show_default=True)
+@click.option(
+    "--alphabet",
+    "-a",
+    default=string.ascii_lowercase + string.digits,
+    help="Alphabet to use",
+    show_default=True,
+)
+@click.option(
+    "--use-punctuation",
+    "-p",
+    default=False,
+    is_flag=True,
+    help="Use punctuation in alphabet",
+)
+@click.option(
+    "--use-uppercase",
+    "-u",
+    default=False,
+    is_flag=True,
+    help="Use uppercase in alphabet",
+)
 def main(size: int, alphabet: str, use_uppercase: bool, use_punctuation: bool):
     if use_uppercase:
         alphabet += string.ascii_uppercase
@@ -19,5 +37,5 @@ def main(size: int, alphabet: str, use_uppercase: bool, use_punctuation: bool):
     print(generate(alphabet, size))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
